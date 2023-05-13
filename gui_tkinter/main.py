5a9f170cd8e50a2d28009f01313b0be2effbc891
Suselkowy <jakubsus77@gmail.com>
2023-05-13 14:35:10 +0200

    tkinter changes

--- gui_tkinter/main.py
+++ gui_tkinter/main.py
@@ -1,5 +1,6 @@
 import socket
 import tkinter as tk
+import ttkbootstrap as ttk
 from tkinter import messagebox, scrolledtext, TOP, NW
 from helpers import receive_data, SendDataType
 import threading
@@ -18,46 +19,46 @@ PORT = 65432  # The port used by the server
 running = True
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
 
 
-class SampleApp(tk.Tk):
+class SampleApp(ttk.Window):
 
     def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
+        ttk.Window.__init__(self, *args, themename='darkly', **kwargs)
 
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
         self.container = tk.Frame(self)
         self.container.pack(side="top", fill="both", expand=True)
         self.container.grid_rowconfigure(0, weight=1)
         self.container.grid_columnconfigure(0, weight=1)
-        self.resizable(0, 0)
+        # self.resizable(0, 0)
         self.s = s
+        self.curr_frame = None
 
         self.width, self.height = 500, 500
         self.geometry(f"{self.width}x{self.height}")
-
+        self.minsize(self.width, self.height)
         self.frames = {}
-        for F in (LoginPage, ChooseGamePage, BaccaratGamePage , DiceGamePage, RouletteGamePage, BingoGamePage, BlackjackGamePage):
+
+        for F in (
+                LoginPage, ChooseGamePage, BaccaratGamePage, DiceGamePage, RouletteGamePage, BingoGamePage,
+                BlackjackGamePage):
             page_name = F.__name__
             frame = F(parent=self.container, controller=self, width=self.winfo_height(), height=self.winfo_height())
             if page_name == "GamePage":
                 self.handle_function = self.empty_handler
             self.frames[page_name] = frame
             self.update_idletasks()
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
-            frame.grid(row=0, column=0, sticky="nsew")
-
+            # frame.grid(row=0, column=0, sticky="nsew")
         self.show_frame("LoginPage")
 
     def show_frame(self, page_name):
+        if self.curr_frame is not None:
+            self.curr_frame.grid_forget()
         frame = self.frames[page_name]
+        frame.grid(row=0, column=0, sticky="nsew")
+        self.curr_frame = frame
         self.handle_function = frame.handle_message
-        frame.tkraise()
+        # frame.tkraise()
 
     def empty_handler(self, message):
         pass
@@ -77,27 +78,43 @@ class SampleApp(tk.Tk):
         print("not running")
 
 
-class LoginPage(tk.Frame):
+class LoginPage(ttk.Frame):
 
     def __init__(self, parent, controller, width, height):
-        tk.Frame.__init__(self, parent, width=width, height=height)
+        ttk.Frame.__init__(self, parent, width=width, height=height)
         self.controller = controller
 
-        self.login_header = tk.Label(self, text="Enter your username")
-        self.login_header.pack(pady=20)
-        self.login_input = tk.Entry(self)
-        self.login_input.pack(pady=20)
-        self.login_button = tk.Button(self, text='Login', command=self.login)
-        self.login_button.pack(pady=20)
+        self.main_frame = ttk.Frame(self)
+
+        self.login_header = ttk.Label(self.main_frame, text="Enter your username")
+        self.login_header.pack(anchor='center', expand=True)
+
+        self.login_input_value = tk.StringVar()
+        self.login_input = ttk.Entry(self.main_frame, textvariable=self.login_input_value)
+        self.login_input.pack(pady=40, anchor='center', expand=True)
+
+        self.login_button = ttk.Button(self.main_frame, text='Login', command=self.login)
+        self.login_button.pack(pady=30, anchor='center', expand=True)
+
+        self.main_frame.place(relx=0.5, rely=0.5, anchor='center')
 
     def login(self):
-        login_value = self.login_input.get()
-        print(login_value)
-        if login_value is not None:
-            s.send(bytes(login_value, "utf-8"))
-            receive_thread = threading.Thread(target=self.controller.receive_messages)
-            receive_thread.start()
-            self.controller.show_frame("ChooseGamePage")
+        value = self.login_input_value.get()
+        print(value)
+        try:
+            if value is not None:
+                s.connect((HOST, PORT))
+                s.send(bytes(value, "utf-8"))
+                receive_thread = threading.Thread(target=self.controller.receive_messages)
+                receive_thread.start()
+                self.controller.show_frame("ChooseGamePage")
+        except:
+            self.show_error_message("Could not connect to server, try again")
+
+    def show_error_message(self, message):
+        error = ttk.Label(text=message, bootstyle="danger")
+        error.place(relx=0.5, rely=0.2, anchor='n')
+        error.after(3000, lambda: error.place_forget())
 
     def handle_message(self, data):
         pass
@@ -110,16 +127,28 @@ class ChooseGamePage(tk.Frame):
         buttons = []
         self.controller = controller
         self.parent = parent
-        button_w = self.controller.winfo_width() // 3
-        button_h = self.controller.winfo_height() // 2
+        # button_w = self.controller.winfo_width() // 3
+        # button_h = self.controller.winfo_height() // 2
 
         curr_row = 0
         curr_col = 0
         i = 0
 
+        self.rowconfigure((0, 1, 2), weight=1, uniform='a')
+        self.columnconfigure(0, weight=1, uniform='a')
+
+        self.header = ttk.Frame(self)
+        self.main = ttk.Frame(self)
+
+        self.main.rowconfigure((0, 1), weight=1, uniform='a')
+        self.main.columnconfigure((0, 1, 2), weight=1, uniform='a')
+
+        self.header.grid(row=0, column=0)
+        self.main.grid(row=1, column=0, rowspan=2)
+
         for game in GAMES:
-            buttons.append(tk.Button(self, text=f"{game}", width=17, height=9))
-            buttons[i].grid(row=curr_row, column=curr_col, padx=8, pady=8, ipadx=10, ipady=40)
+            buttons.append(tk.Button(self.main, text=f"{game}"))
+            buttons[i].grid(row=curr_row, column=curr_col, sticky='nsew')
             buttons[i].bind(f"<Button-1>", self.callback)
 
             i += 1
@@ -139,6 +168,7 @@ class ChooseGamePage(tk.Frame):
         print(f"play {name}")
         s.send(bytes(f"play {name}", 'utf-8'))
         self.controller.show_frame(name.title() + "GamePage")
+
 
 if __name__ == "__main__":
     app = SampleApp()
